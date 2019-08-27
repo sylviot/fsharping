b0fec0041ad8b2faccab10faa845ab08e1ae3735
sylviot <sylvio.junior@globalweb.com.br>
2019-08-27 10:34:19 -0400

    Chapter 4

---
+++ Sandbox/fsharpforfunandprofit/Chapter4.fs
@@ -0,0 +1,24 @@
+﻿module Chapter4
+    let rec quicksort list =
+        match list with
+        | [] -> []
+        | head::tail ->
+            let fn op h t = t |> List.filter( (op) h)
+            List.concat [
+                quicksort( fn (>) head tail)
+                [head];
+                quicksort( fn (<=) head tail)
+            ]
+
+(* Version 2
+    quicksort( tail |> List.filter( (>) head ) );
+    quicksort( tail |> List.filter( (<=) head ) );
+*)
+
+(* Version 1
+    let smallerElements =
+        quicksort( tail |> List.filter( (>) head ) )
+    let largerElements =
+        quicksort( tail |> List.filter( (<=) head ) ) // fun e -> e >= head | head <= x'
+    List.concat [smallerElements; [head]; largerElements]
+*)

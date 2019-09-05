0d90cff8a3e0af5dcc3741fd3376d2c6cf49f9df
sylviot <sylvio.junior@globalweb.com.br>
2019-09-05 13:28:01 -0400

    Chapter 16

---
+++ Sandbox/fsharpforfunandprofit/Chapter16.fs
@@ -0,0 +1,62 @@
+﻿module Chapter16
+    let add x y = x + y
+    
+    let normalUseAdd = add 1 2
+
+    let add42 = add 42
+
+    let genericLogger anyFunc input =
+        printfn "input is %A" input
+        let result = anyFunc input
+        printfn "result %A" result
+        result
+
+    let genericLogger2 before after anyFunc input =
+        before input
+        let result = anyFunc input
+        after result
+        result
+
+    let add1 input = input + 1
+
+    genericLogger2
+        (fun x -> printf "started with=%i" x)
+        (fun x -> printfn " ended with=%i" x)
+        add1
+        2
+
+    let add1WithConsoleLogging =
+        genericLogger2
+            (fun x -> printf "input=%i. " x)
+            (fun x -> printfn " result=%i" x)
+            add1
+            // Last parameter NOT defined here yet!
+
+    let result1 = add1WithConsoleLogging 1
+    let resultList = [1 .. 5] |> List.map add1WithConsoleLogging
+    
+    #if CSHARP
+    public class GenericLoggerHelper<TInput, TResult>
+    {
+        public TResult GenericLogger(Action<TInput> before, Action<TResult> after, Func<TInput, TResult> aFunc, TInput input)
+        {
+            before(input);
+            var result = aFunc(input);
+            after(result);
+
+            return result;
+        }
+    }
+
+    [Test]
+    public void TesteGenericLogger()
+    {
+        var sut = new GenericLoggerHelper<int, int>();
+        sut.GenericLogger(
+            x => Console.Write("input={0}", x),
+            x => Console.WriteLine(" result={0}", x),
+            x => x + 1,
+            1
+        );
+    }
+    #endif

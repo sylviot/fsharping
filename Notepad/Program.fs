056861c66d6bec3c717395ac04f41a34eee8da5d
sylviot <sylvio.tavares@hotmail.com>
2019-09-22 02:35:04 -0400

    Notepad basic

--- Notepad/Program.fs
+++ Notepad/Program.fs
@@ -1,38 +1,87 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿#nowarn "20"
 
 open System
 open System.Windows.Forms
+open System.IO
 
-[<EntryPoint; STAThread>]
-let main argv =
-    let form = new Form(Width = 400, Height = 300, Text = "Notepad")
-    let windowNewMenu_Click = (fun (obj:Object) (x:EventArgs) -> printfn "Clicked")
-    let arquivoFechar_Click = (fun (obj:Object) (x:EventArgs) -> form.Close())
+let mutable file = None
+let openFile filename = File.ReadAllText(filename)
+let saveFile filename contents = File.WriteAllText(filename, contents)
 
-    let ms = new MenuStrip()
-    let arquivo = new ToolStripMenuItem("Arquivo")
-    let arquivoNovo = new ToolStripMenuItem("Novo", null, new EventHandler(windowNewMenu_Click))
-    let arquivoSalvar = new ToolStripMenuItem("Salvar", null, new EventHandler(windowNewMenu_Click))
-    let arquivoFechar = new ToolStripMenuItem("Fechar", null, new EventHandler(arquivoFechar_Click))
-    arquivo.DropDownItems.Add(arquivoNovo) |> ignore
-    arquivo.DropDownItems.Add(arquivoSalvar) |> ignore
-    arquivo.DropDownItems.Add(arquivoFechar) |> ignore
-
-    ms.Dock <- DockStyle.Top
-    ms.Items.Add(arquivo) |> ignore
-    
-
+let fnMenu() = (
+    new ToolStripMenuItem("Arquivo")
+)
+let fnMenuChildren(title, fn) = (
+    new ToolStripMenuItem(title, null, new EventHandler(fn))
+)
+let fnTextbox() = (
     let textbox = new TextBox()
     textbox.AcceptsTab <- true
     textbox.Dock <- DockStyle.Fill
     textbox.Location <- Drawing.Point(0, 500)
     textbox.Multiline <- true
     textbox.ScrollBars <- ScrollBars.Vertical
+    textbox.Visible <- false
+    textbox
+)
 
+[<EntryPoint; STAThread>]
+let main argv =
+    let form = new Form(Width = 400, Height = 300, Text = "Notepad")
+    let menu = new MenuStrip()
+    let menuFile = fnMenu()
+
+    let textbox = fnTextbox()
+    let openFileDialog = new OpenFileDialog()
+    let saveFileDialog = new SaveFileDialog()
+
+    openFileDialog.CheckFileExists <- true
+    openFileDialog.CheckPathExists <- true
+
+    saveFileDialog.AddExtension <- true
+    saveFileDialog.DefaultExt <- ".txt"
+
+    let fnShowTextbox() =
+        textbox.Text <- ""
+        textbox.Visible <- true
+        textbox.Focus()
+        |>  ignore
+
+    let fnEdit(filename:string) =
+        file <- Some filename
+        textbox.Text <- openFile filename
+        fnShowTextbox()
+
+    let newFile_click = (fun (obj:Object) (x:EventArgs) -> 
+        fnShowTextbox()
+    )
+    let openFile_click = (fun (obj:Object) (x:EventArgs) ->
+        match openFileDialog.ShowDialog() with
+            | DialogResult.OK -> fnEdit openFileDialog.FileName
+            | _ -> printfn "Nothing"
+    )
+    let trySave() = 
+        match saveFileDialog.ShowDialog() with
+            | DialogResult.OK -> saveFile saveFileDialog.FileName textbox.Text
+            | _ -> printfn "Nothing"
+
+    let saveFile_click = (fun (obj:Object) (x:EventArgs) ->
+        match file with
+        | Some file -> saveFile file textbox.Text |> ignore
+        | None -> trySave() |> ignore
+    )
+    let close_click = (fun (obj:Object) (e:EventArgs) -> form.Close())
+    
     form.SuspendLayout()
 
+    fnMenuChildren ("Novo", newFile_click) |> menuFile.DropDownItems.Add
+    fnMenuChildren ("Abrir", openFile_click) |> menuFile.DropDownItems.Add
+    fnMenuChildren ("Salvar", saveFile_click) |> menuFile.DropDownItems.Add
+    fnMenuChildren ("Fechar", close_click) |> menuFile.DropDownItems.Add
+
+    menu.Items.Add(menuFile)
     form.Controls.Add(textbox)
-    form.Controls.Add(ms)
+    form.Controls.Add(menu)
     
     form.ResumeLayout(false)
     form.PerformLayout()

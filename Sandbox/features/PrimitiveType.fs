c78af131b6c7e09a2175b655e9e8be166337a83b
sylviot <sylvio.junior@globalweb.com.br>
2019-09-23 14:24:09 -0400

    Features tutorial

---
+++ Sandbox/features/PrimitiveType.fs
@@ -0,0 +1,12 @@
+﻿module PrimitiveType
+    let sampleInteger = 1
+    let sampleDecimal = 3.14M
+    let sampleFloat = 3.123456789f
+    
+    let sampleArrayRange = [| 0 .. 99 |]
+    let sampleArray = [| 1; 2; |]
+
+    let sampleListRange = [0..99]
+    let sampleList = [1; 2; 3]
+
+    let sampleListInt_Int = [for i in 0..99 -> (i, i*i)]

1d70dfe5adeb46293f8cd8303bd987797a4ddb86
sylviot <sylvio.junior@globalweb.com.br>
2019-09-24 13:30:17 -0400

    Primitive Type

--- Sandbox/features/PrimitiveType.fs
+++ Sandbox/features/PrimitiveType.fs
@@ -1,7 +1,12 @@
 ﻿module PrimitiveType
+    open System
+
     let sampleInteger = 1
     let sampleDecimal = 3.14M
     let sampleFloat = 3.123456789f
+    let sampleString = "string"
+    let sampleDatetime = DateTime.Now
+
     
     let sampleArrayRange = [| 0 .. 99 |]
     let sampleArray = [| 1; 2; |]

d6fdf443b575269b838e1563ff2172ce60ae8f24
sylviot <sylvio.tavares@hotmail.com>
2019-09-25 23:09:44 -0400

    Project Paint

---
+++ Paint/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+open System.Windows.Forms
+
+[<EntryPoint; STAThread>]
+let main argv =
+    let form = new Form(Width = 400, Height = 300, Text = "Paint")
+    Application.Run form
+    0

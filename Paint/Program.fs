d58693332093a01530fa70923e1bfdffe5eedac2
sylviot <sylvio.junior@globalweb.com.br>
2019-09-30 13:25:07 -0400

    Comment

--- Paint/Program.fs
+++ Paint/Program.fs
@@ -1,6 +1,11 @@
-﻿open System
+﻿(*
+    Paint
+    New File, Save, Close
+    Brush, Erase - Actions
+    Color Pallet
+*)
+open System
 open System.Windows.Forms
-
 [<EntryPoint; STAThread>]
 let main argv =
     let form = new Form(Width = 400, Height = 300, Text = "Paint")
